d610734c7746d54e29e57316a339ef88f9bfe0fc
Tann1c22 <115367712+Tann1c22@users.noreply.github.com>
2023-01-06 15:50:58 +0000

    Code added

---
+++ ControlWork/Program.cs
@@ -0,0 +1,34 @@
+﻿//Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых равна или меньше 3. Первоначальный массив вводится с клавиатуры
+
+Console.WriteLine("Введите количество элементов в массиве ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] array = MassElems(size);
+MassOfElemsLessThanFour(array, size);
+//----------------Методы---------------//
+
+string[] MassElems(int size)
+{
+    string[] arr = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите {i+1} элемент массива ");
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
+
+void MassOfElemsLessThanFour(string[] arr, int size)
+{
+    string[] changedArray = new string[size];
+    int j = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if(arr[i].Length <= 3)
+        {
+            changedArray[j] = arr[i];
+            Console.Write($"{changedArray[j]}");
+            Console.Write(" ");
+            j++;
+        }
+    }
+}
